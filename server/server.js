017e40af6ce8372ad62b6acd582d57bef5d76569
krohitrk1104 <krohitrk1104@gmail.com>
2026-06-15 02:22:41 +0530

    Config dotenv file, Add Port in dot env

--- server/server.js
+++ server/server.js
@@ -1,13 +1,17 @@
-//line 2 & 3 about webcrypto what is this and how it affect during connecting Mongo Database
+//line 2 & 3 about webcrypto what is this and how it affect during connecting Mongo Database (Remore this comment and update during debugging and testing)
 const { webcrypto } = require('crypto');
 global.crypto = webcrypto;
 
+let PORT;
 const app =require('./src/app');
 const connectDB =require('./src/db/db');
+
+//for using .env file var require dotenv package
+require('dotenv').config()
 
 connectDB();
 
 
-app.listen(3000,()=>{
-    console.log("Server is running at port no. 3000");
+app.listen(process.env.PORT || 4000 ,()=>{
+    console.log(`Server is running at port no. ${process.env.PORT || 4000}`);
 })
