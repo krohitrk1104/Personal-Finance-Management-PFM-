e27212f45645e176dca335d3875375232c858a3b
krohitrk1104 <krohitrk1104@gmail.com>
2026-06-15 02:27:02 +0530

    Database Connect Using Mongodb_URL

--- server/src/db/db.js
+++ server/src/db/db.js
@@ -1,7 +1,9 @@
+const { configDotenv } = require('dotenv');
 const mongoose =require('mongoose');
+// require('dotenv').config
 
 async function connectDB(){
-    await mongoose.connect("mongoDB connection string-Key");
+    await mongoose.connect(process.env.MONGODB_URL);
 
     console.log("Connected to DB");
     
