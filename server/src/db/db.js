5214913419cbc85721ef73d65907f3a0555056b3
krohitrk1104 <krohitrk1104@gmail.com>
2026-06-15 04:25:21 +0530

    Error handling in Connecting with Database Using try-catch block

--- server/src/db/db.js
+++ server/src/db/db.js
@@ -3,10 +3,15 @@ const mongoose =require('mongoose');
 // require('dotenv').config
 
 async function connectDB(){
+   try{
     await mongoose.connect(process.env.MONGODB_URL);
 
-    console.log("Connected to DB");
-    
+    console.log("Connected to Mongodb DataBase");
+   }
+   catch(err){
+    console.error("Database connection failed :", err.message );
+    process.exit(1);
+    }
 }
 
 module.exports =connectDB;
