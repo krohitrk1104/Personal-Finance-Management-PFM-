9604904fede03fd2228a39a85f971a8d1380f2e0
krohitrk1104 <krohitrk1104@gmail.com>
2026-06-18 00:36:27 +0530

    app file

--- server/src/app.js
+++ server/src/app.js
@@ -2,10 +2,4 @@ const express =require('express');
 
 const app= express();
 
-//API Create and call testing.
-
-// app.get('/',(req ,res)=>{
-//     res.send("Hello world");
-// } )
-
 module.exports = app;
