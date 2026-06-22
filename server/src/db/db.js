0d93652611e0380c776cde6326f4b4e4345c3d30
krohitrk1104 <krohitrk1104@gmail.com>
2026-06-22 13:40:53 +0530

    fix-databse connectivity error...

--- server/src/db/db.js
+++ server/src/db/db.js
@@ -1,6 +1,11 @@
-const { configDotenv } = require('dotenv');
-const mongoose =require('mongoose');
+//To encounter this error in connecting mongodb 2&3 add==>{"querySrv ECONNREFUSED _mongodb._tcp.cluster-pfm.ruxs5xn.mongodb.net" }
+const dns = require('node:dns');
+dns.setServers(['1.1.1.1', '1.0.0.1']); // Forces Cloudflare DNS
 // require('dotenv').config
+const { configDotenv } = require('dotenv');
+//require mongoose package
+const mongoose =require('mongoose');
+
 
 async function connectDB(){
    try{
